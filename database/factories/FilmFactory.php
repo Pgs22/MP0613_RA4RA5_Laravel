db3bb6056a4edb611edce3a9b4d4b821fa18a269
patricia <patricia.gsbcn@gmail.com>
2026-03-08 11:17:46 +0100

    #35 Factory "mproved"

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -15,16 +15,21 @@ class FilmFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        $randomId = $this->faker->numberBetween(1, 100);
+{
+        $prefixes = ['El secreto de', 'El regreso de', 'Más allá de', 'El último', 'La sombra de', 'Crónicas de', 'Misión en'];
+        $nouns = ['la Galaxia', 'un Imperio', 'la Leyenda', 'el Silencio', 'un Guerrero', 'la Tormenta', 'la Jungla', 'un Destino'];
         
+        $movieName = $this->faker->randomElement($prefixes) . ' ' . $this->faker->randomElement($nouns);
+        $genres = ['action', 'drama', 'romance', 'suspense'];
+        $selectedGenre = $this->faker->randomElement($genres);
+
         return [
-            'name' => $this->faker->sentence(3),
-            'year' => $this->faker->year(),
-            'genre' => $this->faker->randomElement(['suspense', 'action', 'drama', 'romance']),
-            'duration' => $this->faker->numberBetween(80, 180),
+            'name' => $movieName,
+            'year' => $this->faker->numberBetween(1980, 2026),
+            'genre' => $selectedGenre,
+            'duration' => $this->faker->numberBetween(90, 180),
             'country' => $this->faker->country(),
-            'img_url' => "https://picsum.photos/id/{$randomId}/600/400",
+            'img_url' => "https://loremflickr.com/600/400/movie,poster," . $selectedGenre . "/all?lock=" . md5($movieName),
         ];
     }
 }
